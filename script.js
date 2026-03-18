b98405f10516c4c3b93249f6dc33753bf361a07e
noe022 <noeliaperezmojica@gmail.com>
2026-03-18 12:42:54 +0000

    Restore task state

--- script.js
+++ script.js
@@ -3,46 +3,53 @@ const tasksDiv = document.querySelector("#tasks-div");
 const logo = document.querySelector("#logo");
 
 let tasksBackup = JSON.parse(localStorage.getItem("tasks")) || [];
-tasksBackup.forEach(taskText => CreateTasks(taskText));
+tasksBackup.forEach(taskText => CreateTask(taskText));
 
-function CreateTasks(taskText){
-  if(taskText.trim() === "") return;
-
+function CreateTask(taskText){
+  if(taskText.text.trim() === "") return;
   const taskCard = document.createElement('li');  
   taskCard.classList.add('tasks-card');
 
   const button = document.createElement('button');
   button.classList.add('button');
   button.addEventListener('click', function(){
-    if (task.style.textDecoration === "line-through") {
-      task.style.textDecoration = "none";
-      button.textContent = "";
-      button.classList.remove('done');
-    } else {
+    taskText.done = !taskText.done;
+    localStorage.setItem("tasks", JSON.stringify(tasksBackup));
+    if (taskText.done) {
       task.style.textDecoration = "line-through";
-      button.textContent = "✓";
       button.classList.add('done');
+    } else {
+      task.style.textDecoration = "none";
+      button.classList.remove('done');
     }
   });
   taskCard.appendChild(button);
   
   const task = document.createElement('p');
-  task.textContent = taskText;
+  task.textContent = taskText.text;
   taskCard.appendChild(task);
+
+  if (taskText.done) {
+    task.style.textDecoration = "line-through";
+    button.classList.add('done');
+  }
 
   const deleteTask = document.createElement('div');
   deleteTask.classList.add('delete-task');
   deleteTask.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#e97ca8"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg>'
   deleteTask.addEventListener('click', function(){
-    let newArray = [];
-    tasksBackup.forEach(function(oneTaskBackup) {
-      if (oneTaskBackup !== taskText) {
-        newArray.push(oneTaskBackup);
-      }
-    });
-    tasksBackup = newArray;
-    localStorage.setItem("tasks", JSON.stringify(tasksBackup));
-    taskCard.remove();
+    taskCard.classList.add('dissapear');
+    taskCard.addEventListener('animationend', function(){
+      let newArray = [];
+      tasksBackup.forEach(function(oneTaskBackup) {
+        if (oneTaskBackup.text !== taskText.text) {
+          newArray.push(oneTaskBackup);
+        }
+      });
+      tasksBackup = newArray;
+      localStorage.setItem("tasks", JSON.stringify(tasksBackup));
+      taskCard.remove();
+    })
   });
 
   tasksDiv.appendChild(taskCard);
@@ -53,9 +60,10 @@ input.addEventListener('keydown', function(event) {
   if (event.key === 'Enter') {
     const value = input.value.trim();
     if (value === "") return;
-    tasksBackup.push(value);
+    const newTask = {text: value, done: false};
+    tasksBackup.push(newTask);
     localStorage.setItem("tasks", JSON.stringify(tasksBackup));
-    CreateTasks(value);
+    CreateTask(newTask);
     input.value = "";
   }
 })
